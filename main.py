6149002a5d617c8fc26b627eb3ce9694e6136696
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-03-19 12:59:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Sol pulido
+#09/14/23 
+#Di print practice and learn how /n works 
+
+#It wil print of the console 
+print("I like making recipes")
+print("Hello my name is : sol and i'm " , 10+7 , "years old")
+print("today is 9 / 14 /", 2000+23)
+#the \n chracter is a method to kake space between lines
+print("i love computer science! \n\n\n i cannot wait to learn more about pyton")
